2cf10294744690ca940ec64029e33b95c4ab7f34
alexiskeila <Essi@Alexiss-MacBook-Air.local>
2016-12-23 20:40:07 -0500

    portfolio

---
+++ Portfolio.js
@@ -0,0 +1,134 @@
+
+<head>
+<script src="../lib/jquery-1.11.1.min.js"></script>
+<script src="../lib/mousetrap.min.js"></script>
+<script src="main.js"></script>
+// <link href="https://fonts.googleapis.com/css?family=Molle:400i" rel="stylesheet">
+
+<style>
+@font-face {
+    font-family: 'Molle', cursive;
+    src: url('https://fonts.googleapis.com/css?family=Molle:400i');
+    font-weight: normal;
+    font-style: normal;
+
+}
+#container {
+	margin-left: auto;
+	margin-right: auto;
+	max-width: 800px;
+	width: 90%;
+	background-color: rgba(200,200,200,0.5);
+	padding: 2vh 1vw;
+}
+td p {
+	text-align: left;
+	font-size: 1.85vh;
+	font-family: Gabriele;
+	font-weight: bold;
+}
+td {
+	/*height: 23vh;*/
+	padding-right: 14%;
+    padding-left: 1.5em;
+    padding-bottom: 0.7vh;
+    text-indent:-1.5em;
+	vertical-align: top;
+}
+td.keyChar {
+	width:10%;
+	padding-right: 0%;
+	padding-left: 0em;
+    text-indent: 0em;
+}
+body {
+	margin-top: 2%;
+	  background: url(https://media.giphy.com/media/dwaeIbBnF6HBu/giphy.gif) no-repeat center center fixed; 
+	  -webkit-background-size: cover;
+	  -moz-background-size: cover;
+	  -o-background-size: cover;
+	  background-size: cover;
+	font-family: Gabriele;
+	text-align:center;
+}
+a {
+	text-decoration: none;
+}
+a:hover{
+	color:#D3D;
+	/*text-shadow: black 2px 1px*/
+}
+
+img {
+	display:block;
+	margin:4vh auto 2vh auto;
+	max-width: 720px;
+	width: 80%;
+}
+h3 {
+
+	font-size: 1.3em;
+	-webkit-margin-after:0.5vh;
+
+}
+h3 a {
+	text-decoration: none;
+	font-size: 0.75em;
+}
+em {
+	font-size: 0.75em;
+}
+</style>
+</head>
+<body>
+<div id="container">
+<h1 id="title"></h3>
+<h4>A Digital Language Arts II Anthology</h4>
+<h4>~~~~~</h4>
+<h3>< Isaiah Brand ></h3>
+<p>
+<a href="https://www.youtube.com/watch?v=loBy_hOKkGc
+" target="_blank">Space</a><br>
+<a href="https://izzybrand.github.io/week9-twinery/onaclearnight.html" target="_blank">On a Clear Night</a><br>
+<a href="https://twitter.com/ponderthem" target="_blank">@ponderbot</a></p>
+<h3>< Jiajie Chen ></h3>
+<p>
+<a href="http://jiajie-chen.com/litr0210d/final/
+" target="_blank">Windows Loading Screen</a><br>
+<a href="http://jiajie-chen.com/litr0210d/week-5/" target="_blank">Fragile Memes</a><br>
+<a href="http://jiajie-chen.com/litr0210d/week-6/" target="_blank">Bon Jovi Simulator</a></p>
+<h3>< Aubrey DeRonde ></h3>
+<p>
+<a href="https://aderonde.github.io/week6-sound/messagebottle.html
+" target="_blank">Message Bottle</a><br>
+<a href="https://aderonde.github.io/week7-instruments/halloween.html" target="_blank">True Terror 2</a><br>
+<a href="https://aderonde.github.io/cavetwine/cave.html" target="_blank">Scorpion Grass</a></p>
+<h3>< Joe Engelman ></h3>
+<p>
+<a href="http://joengelm.github.io/projects/markov/harrypotter.html" target="_blank">Harry Potter and the...</a><br>
+<a href="https://twitter.com/craymojibot" target="_blank">@craymojibot</a><br>
+<a href="https://aim95.herokuapp.com" target="_blank">AIM 95</a></p>
+<h3>< Andrew Ganem ></h3>
+<p>
+<a href="https://ishmaelstrom.github.io/week7-instruments/poemGen/poemGen.html" target="_blank">Poem Instrument</a><br>
+<a href="https://twitter.com/b0tDyl4n" target="_blank">Bot Dylan</a><br>
+<a href="https://ishmaelstrom.github.io/week2-tracery/listiclebot.html" target="_blank">Listicle Bot</a></p>
+<h3>< Jack Kelly ></h3>
+<p>
+<a href="https://jack-k.github.io/dla/panic" target="_blank">Panic</a><br>
+<a href="https://jack-k.github.io/dla/anotherday" target="_blank">Another Day</a><br>
+<a href="https://jack-k.github.io/dla/maga" target="_blank">Make America Great Again</a></p>
+<h3>< Will Thompson ></h3>
+<p>
+<a href="http://wcthompson.github.io/showcase/incomplete_me/" target="_blank">IncompleteMe</a><br>
+<a href="http://wcthompson.github.io/showcase/macros/" target="_blank">A Collection of Hearty Macros and Memes</a><br>
+</p>
+<h3>< Alexis Viera ></h3>
+<p>
+<a href="https://alexiskeila.github.io/alexiskeila.github.io/Week2-Tracery/poetry.html" target="_blank">No-Bake Sestina</a><br>
+<a href="https://alexiskeila.wordpress.com/" target="_blank">Imagine Macros</a><br>
+<a href="https://alexiskeila.github.io/AdsAnnual16.html" target="_blank">Ads Annual, 2016 Edition</a></p>
+<h3>< Neil Wathore ></h3>
+<p><a href="https://wathoren.github.io/DLA_MENU.html" target="_blank">Nexus</a><br><a href="https://wathoren.github.io/DLA_MENU.html" target="_blank">Staccato</a><br><a href="https://wathoren.github.io/Tracery_Nationwide_Commercial_Neil_Wathore.htm" target="_blank">Death & Profit</a></p>
+</div>
+</body>
